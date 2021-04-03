2b63d0b5e8fe31c54a4818f16c89aeb58ef0bf41
Mark Kim <markuskimius@gmail.com>
2021-04-02 22:38:21 -0400

    define CAFE_WORKDIR, CAFE_LOGDIR, CAFE_TOKENDIR

---
+++ etc/bashrc.d/10-defaults.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##############################################################################
+# CAFE: Organize your Unix packages
+# https://github.com/markuskimius/cafe
+#
+# Copyright (c)2020-2021 Mark K. Kim
+# Released under the Apache license 2.0
+# https://github.com/markuskimius/cafe/blob/master/LICENSE
+##############################################################################
+
+export CAFE_WORKDIR=${CAFE_WORKDIR-$CAFE/work}
+export CAFE_LOGDIR=${CAFE_LOGDIR-$CAFE_WORKDIR/log}
+export CAFE_TOKENDIR=${CAFE_TOKENDIR-$CAFE_WORKDIR/token}
+
